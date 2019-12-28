21c875640068ae484539d0a5bb5ddaea4d785114
doducanh1999 <anhddgch17484@fpt.edu.vn>
2019-12-28 13:13:44 +0700

    first

--- connect.php
+++ connect.php
@@ -1,3 +1,4 @@
 <?php
-$conn = pg_connect("host=ec2-174-129-22-22.compute-1.amazonaws.com port=5432 dbname=d37vvq38jkq80h user=elzexzyerwtdyb password=c1fd8e2de951c2c5e2f0c3ae214f41eed89c6a9d2b7e71569203b2d3699f46cc") or die("Connect Unsuccessful");
+$conn = pg_connect("host=ec2-174-129-22-22.compute-1.amazonaws.com port=5432 dbname=decnp97rd0mqvv
+ user=lxlofzkbnjumwx password=f34e3553cbbb59bd7ef812dad3c8261eef0239fbd1570808843fba9bc3a71613") or die("Connect Unsuccessful");
 ?>
